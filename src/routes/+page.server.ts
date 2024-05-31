67b0a8b5661f4f3a727a611939a2f70cb80aaf61
Shash Comandur <shashank.comandur@gmail.com>
2024-05-30 23:15:13 -0400

    fix: time zone hotfix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,20 +4,20 @@ const month_names = ["January", "February", "March", "April", "May", "June", "Ju
 
 function get_todays_game() {
     const today_date = new Date();
+    today_date.setHours(today_date.getHours() - 4);             // rewind 4 hours, since vercel deployment is in UTC, and the game works in EST
     let month = month_names[today_date.getMonth()].toString()
     let date = today_date.getDate().toString()
-    if (date.length == 1) { date = 0 + date}            // add leading 0 if needed
     let year = today_date.getFullYear().toString()
-
+    if (date.length == 1) { date = 0 + date}            // add leading 0 to date if needed
     const formatted_date = `${month} ${date} ${year}`
     console.log(formatted_date)
+
     return games.filter(item => item.date === formatted_date)
 }
 
 export async function load() {
     const game = get_todays_game()[0]
-    console.log(game)
-
+    
     const price = game.price;
     const img = "https://www.traderjoes.com/" + game.img_path;
     const name = game.name;
